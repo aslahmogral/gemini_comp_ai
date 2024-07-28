a9cdec1b2449323d1bd5455adf8e12b99b902805
ashiquepillsbee <ashique.ck@pillsbee.com>
2024-07-28 12:39:12 +0530

    "Added Google Generative AI dependency and API key setup to main.dart"

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:gemini_comp_ai/screen/home_screen/home_screen.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 
 void main() {
+  final apiKey = Platform.environment['AIzaSyANH2im6noAKtHhHRsPZmQdKrHzICkOtAI'];
+  if (apiKey == null) {
+    throw Exception('AIzaSyANH2im6noAKtHhHRsPZmQdKrHzICkOtAI is not set');
+  }
+  final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: apiKey.toString());
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,4 +27,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
